00760074d54f595018f8a9586aaa14a7849bb5a6
HotLoli <barjinderkalermp@gmail.com>
2026-03-28 20:54:24 +0530

    Made the site nicer

--- streamlit_demo.py
+++ streamlit_demo.py
@@ -1,17 +1,17 @@
 import yfinance as yf
 import streamlit as st
-import pandas as pd
+from datetime import datetime, date
 
-st.write("""
-         # STONKS
-         ## SICK
-         """)
+st.title("Stock Value and Volume Visualizer")
 
-tickerSymbol = 'SGML'
+tickerSymbol = st.text_input("Enter the stock code (Like AAPL)", "SGML")
 
 tickerData = yf.Ticker(tickerSymbol)
 
-tickerDF = tickerData.history(start='2010-5-31', end='2020-5-31')
+start_date = st.date_input("Enter the start date for the chart",datetime(2000,1,1), datetime(1990,1,1),date.today())
+end_date = st.date_input("Enter the end date for the chart",date.today(), datetime(1990,1,1),date.today())
+
+tickerDF = tickerData.history(start=start_date, end='2020-5-31')
 
 st.line_chart(tickerDF.Close)
 st.line_chart(tickerDF.Volume)
